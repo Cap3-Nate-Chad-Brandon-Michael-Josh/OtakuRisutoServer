ae5f289632eb887fdb76ebf404b8032c8ee34db3
Chad Drake <chaddrake1313@yahoo.com>
2020-11-20 12:52:22 -0600

    search lists endpoint completed

--- src/search/seach-service.js
+++ src/search/seach-service.js
@@ -2,6 +2,26 @@ const searchService = {
   getUsers(db) {
     return db("users").select("*");
   },
+  getLists(db) {
+    return db("anime_list").select("*");
+  },
+  getOwner(db, user_id) {
+    return db("users").select("*").where({ user_id }).first();
+  },
+  filterListsByName(lists, term) {
+    let result = [];
+    for (let i = 0; i < lists.length; i++) {
+      if (lists[i].name.includes(term)) {
+        result.push({
+          list_id: lists[i].list_id,
+          user_id: lists[i].user_id,
+          name: lists[i].name,
+          private: lists[i].private,
+        });
+      }
+    }
+    return result;
+  },
   filterUsersByName(users, term) {
     let result = [];
     for (let i = 0; i < users.length; i++) {
--- src/search/search-router.js
+++ src/search/search-router.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const searchService = require("./seach-service");
-const requireAuth = require("../middleware/JWT-auth");
+const { requireAuth } = require("../middleware/JWT-auth");
 
 const serachRouter = express.Router();
-//serachRouter.use(requireAuth)
+serachRouter.use(requireAuth);
 
 serachRouter.get("/users/:term", async (req, res, next) => {
   let term = req.params.term;
@@ -16,5 +16,32 @@ serachRouter.get("/users/:term", async (req, res, next) => {
   }
   res.json(result);
 });
-
+serachRouter.get("/lists/:term", async (req, res, next) => {
+  let term = req.params.term;
+  let lists = await searchService.getLists(req.app.get("db"));
+  let filteredList = searchService.filterListsByName(lists, term);
+  let result = [];
+  for (let i = 0; i < filteredList.length; i++) {
+    if (!filteredList[i].private) {
+      let dbOwner = await searchService.getOwner(
+        req.app.get("db"),
+        filteredList[i].user_id
+      );
+      result.push({
+        list_id: filteredList[i].list_id,
+        name: filteredList[i].name,
+        owner: {
+          username: dbOwner.username,
+          user_id: dbOwner.user_id,
+        },
+      });
+    }
+  }
+  if (result.length === 0) {
+    return res
+      .status(400)
+      .json({ error: "Nothing found matching search criteria" });
+  }
+  res.json(result);
+});
 module.exports = serachRouter;
