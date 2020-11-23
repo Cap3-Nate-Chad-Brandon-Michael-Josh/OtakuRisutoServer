b496f01c1106ef894972f4ed2591931208a8e38a
Chad Drake <chaddrake1313@yahoo.com>
2020-11-23 15:53:23 -0600

    cleaning before merging to master

--- test/search-endpoint.test.js
+++ test/search-endpoint.test.js
@@ -4,7 +4,7 @@ const app = require("../src/app");
 const helpers = require("./test-helpers");
 const { expect } = require("chai");
 
-describe.only("search endpoint", () => {
+describe("search endpoint", () => {
   let db;
   const testUsers = helpers.makeUsersArray();
   const testUser = testUsers[0];
