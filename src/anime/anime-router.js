285910adda2635acdfcefa0e8c04fd29b1b079f4
Chad Drake <chaddrake1313@yahoo.com>
2020-11-30 13:37:44 -0600

    added unhappy path testing, as well as more validation on anime endpoints

--- src/anime/anime-router.js
+++ src/anime/anime-router.js
@@ -1,35 +1,35 @@
-const express = require("express");
-const animeService = require("./anime-service");
-const { requireAuth } = require("../middleware/JWT-auth");
+const express = require('express');
+const animeService = require('./anime-service');
+const { requireAuth } = require('../middleware/JWT-auth');
 
 const animeRouter = express.Router();
 animeRouter.use(requireAuth);
 
-animeRouter.post("/", async (req, res, next) => {
+animeRouter.post('/', async (req, res, next) => {
   let { anime, list_id } = req.body;
   if (!anime) {
-    return res.status(400).json({ error: "Missing anime" });
+    return res.status(400).json({ error: 'Missing anime' });
   }
   if (!list_id) {
-    return res.status(400).json({ error: "Missing list_id" });
+    return res.status(400).json({ error: 'Missing list_id' });
   }
-  let owner = await animeService.getListOwner(req.app.get("db"), list_id);
+  let owner = await animeService.getListOwner(req.app.get('db'), list_id);
 
   if (owner.user_id !== req.user.user_id) {
-    return res.status(401).json({ error: "Unauthorized request" });
+    return res.status(401).json({ error: 'Unauthorized request' });
   }
   let exists = await animeService.hasAnimeWithTitle(
-    req.app.get("db"),
+    req.app.get('db'),
     anime.title
   );
   if (!exists) {
     await animeService.addAnime(
-      req.app.get("db"),
+      req.app.get('db'),
       animeService.serializeAnime(anime)
     );
   }
   let dbAnime = await animeService.getAnimeByTitle(
-    req.app.get("db"),
+    req.app.get('db'),
     anime.title
   );
 
@@ -37,29 +37,32 @@ animeRouter.post("/", async (req, res, next) => {
     list_id,
     anime_id: dbAnime[0].anime_id,
   };
-  animeService.addListAnime(req.app.get("db"), listAnime);
+  animeService.addListAnime(req.app.get('db'), listAnime);
   res.status(201).json({ anime });
 });
-animeRouter.delete("/", async (req, res, next) => {
+animeRouter.delete('/', async (req, res, next) => {
   let { list_anime_id } = req.body;
   if (!list_anime_id) {
-    return res.status(400).json({ error: "Missing list_anime_id" });
+    return res.status(400).json({ error: 'Missing list_anime_id' });
+  }
+  if (typeof list_anime_id !== 'number') {
+    return res.status(400).json({ error: 'Invalid list_anime_id' });
   }
   let list_id = await animeService.getLocation(
-    req.app.get("db"),
+    req.app.get('db'),
     list_anime_id
   );
   if (!list_id) {
-    return res.status(400).json({ error: "list_anime not found" });
+    return res.status(400).json({ error: 'list_anime not found' });
   }
   let owner = await animeService.getListOwner(
-    req.app.get("db"),
+    req.app.get('db'),
     list_id.list_id
   );
   if (owner.user_id !== req.user.user_id) {
-    return res.status(401).json({ error: "Unauthorized request" });
+    return res.status(401).json({ error: 'Unauthorized request' });
   }
-  await animeService.deleteListAnime(req.app.get("db"), list_anime_id);
+  await animeService.deleteListAnime(req.app.get('db'), list_anime_id);
   res.status(204).send(`Deleted ${list_anime_id}`);
 });
 module.exports = animeRouter;
--- test/anime-endpoint.test.js
+++ test/anime-endpoint.test.js
@@ -39,6 +39,28 @@ describe('anime endpoint', () => {
     beforeEach('insert anime-lists', () => {
       return helpers.seedAnimeListTable(db, animeListArr);
     });
+    const requiredFields = ['anime', 'list_id'];
+    requiredFields.forEach((field) => {
+      const validAnime = {
+        anime: anime,
+        list_id: animeList.list_id,
+      };
+      it(`Responds 400 when missing ${field}`, () => {
+        delete validAnime[field];
+        return supertest(app)
+          .post('/api/anime')
+          .set('Authorization', helpers.makeAuthHeader(testUser))
+          .send(validAnime)
+          .expect(400, { error: `Missing ${field}` });
+      });
+    });
+    it('Responds 401 when given a list_id belonging to a different user', () => {
+      return supertest(app)
+        .post('/api/anime')
+        .set('Authorization', helpers.makeAuthHeader(testUsers[2]))
+        .send({ anime, list_id: animeList.list_id })
+        .expect(401, { error: 'Unauthorized request' });
+    });
     it('responds with 201 and an anime object after adding it to the database as a list-anime', () => {
       return supertest(app)
         .post('/api/anime')
@@ -79,6 +101,27 @@ describe('anime endpoint', () => {
     });
     beforeEach('insert list_anime', () => {
       return helpers.seedListAnimeTable(db, listAnimeArr);
+    });
+    it('responds 400 when list_anime_id is not provided', () => {
+      return supertest(app)
+        .del('/api/anime')
+        .set('Authorization', helpers.makeAuthHeader(testUser))
+        .send({ foo: 'bar' })
+        .expect(400, { error: 'Missing list_anime_id' });
+    });
+    it('responds 400 when list_anime_id not correct data type', () => {
+      return supertest(app)
+        .del('/api/anime')
+        .set('Authorization', helpers.makeAuthHeader(testUser))
+        .send({ list_anime_id: 'this is wrong' })
+        .expect(400, { error: 'Invalid list_anime_id' });
+    });
+    it('responds 400 when list_anime_id not in database', () => {
+      return supertest(app)
+        .del('/api/anime')
+        .set('Authorization', helpers.makeAuthHeader(testUser))
+        .send({ list_anime_id: 9000 })
+        .expect(400, { error: 'list_anime not found' });
     });
     it('responds with 204 when list_anime is successfully deleted', () => {
       return supertest(app)
